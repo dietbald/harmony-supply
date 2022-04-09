dbcdba96e6c07ab470280161c66847ff33d75bfe
Intrepid-node <intrepid.node@gmail.com>
2022-04-09 13:00:18 -0400

    Particles
    Better FPS.

--- frontend/src/components/ConfigParticles/particlesConfig.js
+++ frontend/src/components/ConfigParticles/particlesConfig.js
@@ -1,116 +1,84 @@
 const particlesConfig = {
+  
+  fps_limit: 60,
   "fullScreen": {
     "enable": true,
     "zIndex": -1
   },
-  "particles": {
-    "number": {
-      "value": 80,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#ffffff"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#ffffff"
+  interactivity: {
+    detect_on: "window",
+  
+    events: {
+      onclick: { enable: true, mode: "push" },
+      onhover: {
+        enable: true,
+        mode: "attract",
+        parallax: { enable: false, force: 60, smooth: 10 }
       },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
+      resize: true
     },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 2,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 1,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": .8,
-      "direction": "random",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": true,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
+    modes: {
+      push: { quantity: 4 },
+      attract: { distance: 200, duration: 0.4, factor: 5 }
     }
   },
-  "interactivity": {
-    "detect_on": "window",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "repulse"
-      },
-      "onclick": {
-        "enable": false,
-        "mode": "push"
-      },
-      "resize": true
+  particles: {
+    color: { value: "#ffffff" },
+    line_linked: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.4,
+      width: 1
     },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
-        }
+    move: {
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
+      bounce: false,
+      direction: "none",
+      enable: true,
+      out_mode: "out",
+      random: false,
+      speed: 2,
+      straight: false
+    },
+    number: { density: { enable: true, value_area: 800 }, value: 80 },
+    opacity: {
+      anim: { enable: false, opacity_min: 0.1, speed: 1, sync: false },
+      random: false,
+      value: 0.5
+    },
+    shape: {
+      character: {
+        fill: false,
+        font: "Verdana",
+        style: "",
+        value: "*",
+        weight: "400"
       },
-      "bubble": {
-        "distance": 300,
-        "size": 8,
-        "duration": 1.5,
-        "opacity": 1,
-        "speed": 3
+      image: {
+        height: 100,
+        replace_color: true,
+        src: "images/github.svg",
+        width: 100
       },
-      "repulse": {
-        "distance": 150,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
+      polygon: { nb_sides: 5 },
+      stroke: { color: "#000000", width: 0 },
+      type: "circle"
+    },
+    size: {
+      anim: { enable: false, size_min: 0.1, speed: 40, sync: false },
+      random: true,
+      value: 5
     }
   },
-  "retina_detect": true
-};
+  polygon: {
+    draw: { enable: false, lineColor: "#ffffff", lineWidth: 0.5 },
+    move: { radius: 10 },
+    scale: 1,
+    type: "none",
+    url: ""
+  },
+  retina_detect: true
+}
   
   export default particlesConfig;
